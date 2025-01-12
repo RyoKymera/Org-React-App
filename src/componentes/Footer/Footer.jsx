eaaec5da392aed59390bf7ae17914b5c70f5ee5b
Diego Mateus <fcdiegomateus@gmail.com>
2025-01-12 14:00:59 -0500

    Ajuste de imágenes corrección error

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -1,16 +1,16 @@
 import "./Footer.css"
 
 const Footer = () => {
-    return <footer className="footer" style={{ backgroundImage: "url(./public/img/Footer.png)"}}> 
+    return <footer className="footer" style={{ backgroundImage: "url(./img/Footer.png)"}}> 
         <div className='redes'>
             <a href='https://www.linkedin.com/in/diego-o-mateus'>
-                <img src="./public/img/linkedin.png" alt='Linkedin' />
+                <img src="./img/linkedin.png" alt='Linkedin' />
             </a>
             <a href='https://github.com/RyoKymera'>
-                <img src="./public/img/github.png" alt='Github' />
+                <img src="./img/github.png" alt='Github' />
             </a>
         </div>
-        <img src="./public/img/Logo.png" alt='org' />
+        <img src="./img/Logo.png" alt='org' />
         <strong>Desarrollado por Diego Mateus</strong>
     </footer>
 }
--- src/componentes/MiOrg/MiOrg.jsx
+++ src/componentes/MiOrg/MiOrg.jsx
@@ -4,7 +4,7 @@ const MiOrg = (props) => {
 
     return <section className="orgSection">
         <h3 className="title">Mi Organización</h3>
-        <img src="./public/img/add.png" alt="add" onClick={props.cambiarMostrar}/>
+        <img src="./img/add.png" alt="add" onClick={props.cambiarMostrar}/>
     </section>
 }
 
