200e885f1fd6ba06fe785fa65a61671809aa0a54
Diego Mateus <fcdiegomateus@gmail.com>
2025-01-12 13:58:52 -0500

    Ajuste deploy GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dist/", //para servidor local usa "/dist"
+  base: "/Org-React-App/", //para servidor local usa "/dist"
   plugins: [react()],
 })
